c86f1cd2d802ef738f24498bc52f9b10c425aef4
Craig Bowman <cbowman@appdev.yearup.org>
2024-10-30 19:50:20 -0500

    1A lab 1 completed

---
+++ Functions/substrings.py
@@ -0,0 +1,91 @@
+# Define known values
+full_name = 'John Doe'
+
+# Calculate the unknown
+print(full_name.find(' '))
+first_name = full_name[0:4]
+last_name = full_name[5:]
+
+# Display the results
+print(full_name)
+print(first_name)
+print(last_name)
+
+# Different value
+# Define known values
+full_name = 'Peyton Manning'
+
+# Calculate the unknown
+print(full_name.find(' '))
+first_name = full_name[0:6]
+last_name = full_name[7:]
+
+# Display the results
+print(full_name)
+print(first_name)
+print(last_name)
+
+
+# Different value
+# Define known values
+full_name = 'Patrick Mahomes'
+
+# Calculate the unknown
+print(full_name.find(' '))
+first_name = full_name[0:7]
+last_name = full_name[8:]
+
+# Display the results
+print(full_name)
+print(first_name)
+print(last_name)
+
+
+# CHALLENGE ACCEPTED
+# Define known values
+full_name = 'Lorde'
+
+# Calculate the unknown
+print(full_name.find(' '))      # output was -1 which means substring not found, with one name only there's only one way to display it
+first_name = full_name[0:]
+last_name = full_name[0:]
+
+# Display the results
+print(full_name)
+print(first_name)
+print(last_name)
+
+
+# Billie Eilish
+# Define known values
+full_name = 'Billie Eilish'
+
+# Calculate the unknown
+print(full_name.find(' '))
+first_name = full_name[0:6]
+last_name = full_name[7:]
+
+# Display the results
+print(full_name)
+print(first_name)
+print(last_name)
+
+
+# Megan Thee Stallion
+# Define known values
+full_name = 'Megan Thee Stallion'
+
+# Calculate the unknown
+print(full_name.find(' '))
+print(full_name.rfind(' '))
+first_name = full_name[0:5]
+middle_name = full_name[6:10]
+last_name = full_name[11:]
+
+# Display the results
+print(full_name)
+print(first_name)
+print(middle_name)
+print(last_name)
+
+
