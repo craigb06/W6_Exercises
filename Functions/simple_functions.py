ab6ca1ee8e4a06242bdea5b1987959ab0dfc7e84
Craig Bowman <cbowman@appdev.yearup.org>
2024-11-03 18:33:57 -0600

    1C lab 1 completed

--- Functions/simple_functions.py
+++ Functions/simple_functions.py
@@ -1,10 +1,24 @@
 # Define three functions
 # favorite_things
 def favorite_things(name, fav_movie, fav_artist):
-    return(f'My name is {name}\nMy favorite movie is {fav_movie}\nMy favorite artist is {fav_artist}')
+    return f'My name is {name}\nMy favorite movie is {fav_movie}\nMy favorite artist is {fav_artist}'
 
 print(favorite_things('Craig Bowman', 'Friday Night Lights', 'Chance The Rapper'))
 
 # why_im_here
 def why_im_here():
-    return f''
+    return f'I am here because I have an affinity for math and I am naturally curious. I wasn\'t made for Data Analytics, it was made for me!'
+
+# creating the function a dfiferent way
+def why_im_here2(reason):
+    return reason
+
+print(why_im_here())
+print(why_im_here2('DA was made for me'))
+
+# favorite_place
+def favorite_place(place, why):
+    return f'One of my favorite places to visit is {place}, because {why}'
+
+print(favorite_place('California', 'the weather is always good'))
+print(favorite_place('Downtown Chicago', 'I love the atmosphere'))
