faf32408cbd837ee9394adc777c47790ec927a27
Craig Bowman <cbowman@appdev.yearup.org>
2024-11-05 19:02:21 -0600

    2A lab 1 started *note to self go back and try name game bonuses

---
+++ Classes/restaurants.py
@@ -0,0 +1,6 @@
+# create class called restaurant
+class Restaurant:
+    '''Restaurant descirption'''
+    def __init__(self, rest_name, food_type):
+        self.rest_name = rest_name
+        self.food_type = food_type
